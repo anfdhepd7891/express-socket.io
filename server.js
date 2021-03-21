6e8ca190e39c3e70acceea5ae8f9557a1ebb34e8
anfdhepd7891 <anfdhepd7891@gmail.com>
2021-03-21 22:55:05 +0900

    efef

--- server.js
+++ server.js
@@ -1,122 +1,158 @@
-'use strict'
-
-const express = require('express')
-const morgan = require('morgan')
-const http = require('http')
-const socketIO = require('socket.io')
-const cors = require('cors');
-const WebSocketWrapper = require("ws-wrapper");
+const http = require('http');
 const WebSocket = require('ws');
-const ccxws = require("ccxws");
-module.exports = function createServer() {
+const url = require('url');
 
+const server = http.createServer();
+const wss1 = new WebSocket.Server({ noServer: true });
+const wss2 = new WebSocket.Server({ noServer: true });
 
-    const app = express()
-    app.use(cors());
-    app.options('*', cors());
-    const upbit = new ccxws.upbit();
+wss1.on('connection', function connection(ws) {
+    // ...
+});
 
+wss2.on('connection', function connection(ws) {
+    // ...
+});
 
-    const wss = new WebSocket.Server({ noServer: true });
-    const server = http.Server(app)
+server.on('upgrade', function upgrade(request, socket, head) {
+    const pathname = url.parse(request.url).pathname;
 
-    wss.on('connection', function connection(ws, request, client) {
-        ws.on('message', function message(msg) {
-            console.log(`Received message ${msg} from user ${client}`);
-
-            ws.send('something');
+    if (pathname === '/foo') {
+        wss1.handleUpgrade(request, socket, head, function done(ws) {
+            wss1.emit('connection', ws, request);
         });
-    });
-
-
-    const market = {
-        id: "KRW-ETH", // remote_id used by the exchange
-        base: "KRW", // standardized base symbol for Bitcoin
-        quote: "ETH", // standardized quote symbol for Tether
-    };
-    server.listen(80, function() {
-        console.log("Server started on port 80")
-    })
-
-
-    app.use(morgan('dev'))
-    app.get('/', function(req, res) {
-        res.send("HELLO")
-    })
-
-    upbit.on("Ticker", (Ticker, market) => io.emit('Ticker', Ticker));
-    upbit.on("l2snapshot", (snapshot, market) => io.emit('snapshot', snapshot, market));
-    upbit.on("trade", (trade, market) => io.emit('trade', trade));
-
-
-    upbit.subscribeTicker(market);
-    upbit.subscribeTrades(market);
-    upbit.subscribeLevel2Snapshots(market);
-
-
-    function tradeServerConnect() {
-        var socketvv = new WebSocketWrapper(new WebSocket("wss://stream.bybit.com/realtime"));
-        // var sss = JSON.stringify([{ "ticket": "fiwwefwefwecjfoew" }, { "type": "trade", "codes": ["KRW-BTC", "KRW-ETH"] }])
-        // socketvv.binaryType = 'arraybuffer';
-        socketvv.send('{"op": "subscribe", "args": ["trade.ETHUSD"]}');
-
-
-        socketvv.on("message", (from, msg) => {
-            try {
-
-
-                const data = JSON.parse(msg);
-                console.log(data);
-
-
-                // io.emit('chat message', data);
-            } catch (e) {
-
-            }
+    } else if (pathname === '/bar') {
+        wss2.handleUpgrade(request, socket, head, function done(ws) {
+            wss2.emit('connection', ws, request);
         });
-
-        socketvv.on("disconnect", () => {
-            console.log("bye")
-            setTimeout(() => {
-
-                tradeServerConnect();
-            }, 1000);
-
-
-
-        });
+    } else {
+        socket.destroy();
     }
+});
+
+server.listen(80);
 
 
 
-    // tradeServerConnect();
+// 'use strict'
+
+// const express = require('express')
+// const morgan = require('morgan')
+// const http = require('http')
+// const socketIO = require('socket.io')
+// const cors = require('cors');
+// const WebSocketWrapper = require("ws-wrapper");
+// const WebSocket = require('ws');
+// const ccxws = require("ccxws");
+// module.exports = function createServer() {
 
 
-    // io.on('connection', function(socket) {
-    //     console.log(socket);
-    //     const serverMessage = { message: "PING" }
-    //     let count = 99911;
-    //     // socket.emit("server-ping", serverMessage)
-
-    //     socket.on("client-pong", (data) => {
-    //         console.log(data.message)
-    //         if (count > 0) {
-    //             socket.emit("server-ping", count)
-    //             count--
-    //         }
-    //     })
-
-    //     socket.on("Ticker", (data) => {
-    //         var eeee
-    //         upbit._constructTicker(eeee, market);
-
-    //         socket.emit("Ticker", eeee)
-
-    //     })
+//     const app = express()
+//     app.use(cors());
+//     app.options('*', cors());
+//     const upbit = new ccxws.upbit();
 
 
-    // })
+//     const wss = new WebSocket.Server({ noServer: true });
+//     const server = http.Server(app)
 
-    return server
+//     wss.on('connection', function connection(ws, request, client) {
+//         ws.on('message', function message(msg) {
+//             console.log(`Received message ${msg} from user ${client}`);
 
-}
+//             ws.send('something');
+//         });
+//     });
+
+
+//     const market = {
+//         id: "KRW-ETH", // remote_id used by the exchange
+//         base: "KRW", // standardized base symbol for Bitcoin
+//         quote: "ETH", // standardized quote symbol for Tether
+//     };
+//     server.listen(80, function() {
+//         console.log("Server started on port 80")
+//     })
+
+
+//     app.use(morgan('dev'))
+//     app.get('/', function(req, res) {
+//         res.send("HELLO")
+//     })
+
+//     upbit.on("Ticker", (Ticker, market) => io.emit('Ticker', Ticker));
+//     upbit.on("l2snapshot", (snapshot, market) => io.emit('snapshot', snapshot, market));
+//     upbit.on("trade", (trade, market) => io.emit('trade', trade));
+
+
+//     upbit.subscribeTicker(market);
+//     upbit.subscribeTrades(market);
+//     upbit.subscribeLevel2Snapshots(market);
+
+
+//     function tradeServerConnect() {
+//         var socketvv = new WebSocketWrapper(new WebSocket("wss://stream.bybit.com/realtime"));
+//         // var sss = JSON.stringify([{ "ticket": "fiwwefwefwecjfoew" }, { "type": "trade", "codes": ["KRW-BTC", "KRW-ETH"] }])
+//         // socketvv.binaryType = 'arraybuffer';
+//         socketvv.send('{"op": "subscribe", "args": ["trade.ETHUSD"]}');
+
+
+//         socketvv.on("message", (from, msg) => {
+//             try {
+
+
+//                 const data = JSON.parse(msg);
+//                 console.log(data);
+
+
+//                 // io.emit('chat message', data);
+//             } catch (e) {
+
+//             }
+//         });
+
+//         socketvv.on("disconnect", () => {
+//             console.log("bye")
+//             setTimeout(() => {
+
+//                 tradeServerConnect();
+//             }, 1000);
+
+
+
+//         });
+//     }
+
+
+
+//     // tradeServerConnect();
+
+
+//     // io.on('connection', function(socket) {
+//     //     console.log(socket);
+//     //     const serverMessage = { message: "PING" }
+//     //     let count = 99911;
+//     //     // socket.emit("server-ping", serverMessage)
+
+//     //     socket.on("client-pong", (data) => {
+//     //         console.log(data.message)
+//     //         if (count > 0) {
+//     //             socket.emit("server-ping", count)
+//     //             count--
+//     //         }
+//     //     })
+
+//     //     socket.on("Ticker", (data) => {
+//     //         var eeee
+//     //         upbit._constructTicker(eeee, market);
+
+//     //         socket.emit("Ticker", eeee)
+
+//     //     })
+
+
+//     // })
+
+//     return server
+
+// }
