343a2bf7a128336414ea35c41236a7fc9efa6a49
anfdhepd7891 <anfdhepd7891@gmail.com>
2021-03-21 22:29:50 +0900

    efef

--- server.js
+++ server.js
@@ -1,26 +1,5 @@
-/*******************************************************************************
- * Copyright (c) 2016 Nicola Del Gobbo 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the license at http://www.apache.org/licenses/LICENSE-2.0
- *
- * THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
- * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
- * IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
- * MERCHANTABLITY OR NON-INFRINGEMENT.
- *
- * See the Apache Version 2.0 License for specific language governing
- * permissions and limitations under the License.
- *
- * Contributors - initial API implementation:
- * Nicola Del Gobbo <nicoladelgobbo@gmail.com>
- ******************************************************************************/
-
 'use strict'
 
-/*!
- * Module dependencies
- */
 const express = require('express')
 const morgan = require('morgan')
 const http = require('http')
@@ -30,21 +9,21 @@ const WebSocketWrapper = require("ws-wrapper");
 const WebSocket = require('ws');
 const ccxws = require("ccxws");
 module.exports = function createServer() {
+    const wss = new WebSocket.Server({ port: 80 });
 
     const app = express()
     app.use(cors());
     app.options('*', cors());
     const upbit = new ccxws.upbit();
     const server = http.Server(app)
-    const io = socketIO(server, {
-        cors: {
-            origin: "https://playe.us",
-            methods: ["GET", "POST"],
-            allowedHeaders: ["my-custom-header"],
-            credentials: true
-        }
-    })
 
+    wss.on('connection', function connection(ws) {
+        ws.on('message', function incoming(message) {
+            console.log('received: %s', message);
+        });
+
+        ws.send('something');
+    });
 
 
 
@@ -108,33 +87,33 @@ module.exports = function createServer() {
 
 
 
-    tradeServerConnect();
+    // tradeServerConnect();
 
 
-    io.on('connection', function(socket) {
-        console.log(socket);
-        const serverMessage = { message: "PING" }
-        let count = 99911;
-        // socket.emit("server-ping", serverMessage)
+    // io.on('connection', function(socket) {
+    //     console.log(socket);
+    //     const serverMessage = { message: "PING" }
+    //     let count = 99911;
+    //     // socket.emit("server-ping", serverMessage)
 
-        socket.on("client-pong", (data) => {
-            console.log(data.message)
-            if (count > 0) {
-                socket.emit("server-ping", count)
-                count--
-            }
-        })
+    //     socket.on("client-pong", (data) => {
+    //         console.log(data.message)
+    //         if (count > 0) {
+    //             socket.emit("server-ping", count)
+    //             count--
+    //         }
+    //     })
 
-        socket.on("Ticker", (data) => {
-            var eeee
-            upbit._constructTicker(eeee, market);
+    //     socket.on("Ticker", (data) => {
+    //         var eeee
+    //         upbit._constructTicker(eeee, market);
 
-            socket.emit("Ticker", eeee)
+    //         socket.emit("Ticker", eeee)
 
-        })
+    //     })
 
 
-    })
+    // })
 
     return server
 
